0de3ac3c9eefd3750dacdf5ddc7bb239e1324ce2
rjones024 <rodney.jones@hotmail.com>
2026-07-17 23:01:55 -0500

    fix syntax for blog_http

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "blog" {
 vpc_id = data.aws.vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "blog http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
