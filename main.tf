ed9142239f28ef8b4ddf6d82acdaf2869733ed12
rjones024 <rodney.jones@hotmail.com>
2026-07-17 23:21:45 -0500

    vpc fix 3

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and https in. Allow everything out"
 
-  vpc_id = data.aws.vpc.rj-testlab-vpc.id
+  vpc_id = data.aws.vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
