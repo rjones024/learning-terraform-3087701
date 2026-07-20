37e2c08abac1deb370c5d82800523ab9d666caa2
rjones024 <rodney.jones@hotmail.com>
2026-07-20 17:08:55 -0500

    renamed blog_new to blog ln 35

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-  name = "blog_new"
+  name = "blog"
   
   vpc_id = data.aws_vpc.default.id
     ingress_rules       = ["http-80-tcp","https-443-tcp"]
