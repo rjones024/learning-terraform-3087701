48c0215676b855ad1fe7d0105bfa62a6723415e5
rjones024 <rodney.jones@hotmail.com>
2026-07-23 00:20:21 -0500

    vpc define for blog_sg

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_instance" "blog" {
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
-  
+
   tags = {
     Name = "Learning Terraform"
   }
@@ -50,7 +50,7 @@ module "blog_sg" {
   version = "4.13.0"
   name = "blog"
   
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
     ingress_rules       = ["http-80-tcp","https-443-tcp"]
     ingress_cidr_blocks = ["0.0.0.0/0"]
 
