8d52b0b480ab520ff87a52a78b375f553f215f8d
rjones024 <rodney.jones@hotmail.com>
2026-07-24 22:08:21 -0500

    deploy dev environment

---
+++ environments/dev/moved.tf
@@ -0,0 +1,26 @@
+moved {
+    from = module.blog_vpc
+    to   = module.dev.module.blog_vpc
+}
+
+moved {
+    from = module.blog_sg
+    to   = module.dev.module.blog_sg
+}
+
+moved {
+    from = module.blog-alb
+    to   = module.dev.module.blog-alb
+}
+
+moved {
+    from = aws_lb_target_group.blog
+    to   = module.dev.aws_lb_target_group.blog
+}
+
+moved {
+    from = module.blog_vpc
+    to   = module.dev.blog_autoscaling
+}
+
+
